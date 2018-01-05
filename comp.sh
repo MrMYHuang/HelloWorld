9f49807718b4e968f52cacca8a9df3777808b6b6
myh <myh@live.com>
2018-01-05 22:27:50 +0800

    * Update a script.

--- comp.sh
+++ comp.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-g++ -shared -fPIC HelloWorld.cpp -o libHelloWorld.so
-g++ -L. -lHelloWorld  Main.cpp -o Main
+/opt/anaconda3/bin/x86_64-conda_cos6-linux-gnu-g++ -shared -fPIC -I/opt/anaconda3/include HelloWorld.cpp -o libHelloWorld.so
+# Good to link.
+/opt/anaconda3/bin/x86_64-conda_cos6-linux-gnu-g++ -L/opt/anaconda3/lib -lprotobuf -L. -lHelloWorld  Main.cpp -o Main
 LD_LIBRARY_PATH=. ./Main
 
-/opt/anaconda3/bin/x86_64-conda_cos6-linux-gnu-g++ -shared -fPIC -I/opt/anaconda3/include HelloWorld.cpp -o libHelloWorld.so
-g++ -L/opt/anaconda3/lib -Lprotobuf -L. -lHelloWorld  Main.cpp -o Main
+g++ -shared -fPIC -I/opt/anaconda3/include HelloWorld.cpp -o libHelloWorld.so
+# Faild to link.
+g++ -L/opt/anaconda3/lib -lprotobuf -L. -lHelloWorld  Main.cpp -o Main
